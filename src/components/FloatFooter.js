08810fc08763d792a9f3aa3f5f0511f6528c14b4
Rupinder1161 <[gurpreet1161@gmail.com]>
2020-07-07 13:28:23 +1200

    popover done

--- src/components/FloatFooter.js
+++ src/components/FloatFooter.js
@@ -4,6 +4,44 @@ import Badge from '@material-ui/core/Badge';
 import MailIcon from '@material-ui/icons/Mail';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+import Button from '@material-ui/core/Button';
+
+
+
+const Table= props => (
+    
+    <table>
+    <caption>Order Summary</caption>
+    <thead>
+      <tr>
+        <th scope="col">Food Name</th>
+        <th scope="col">Venue Name</th>
+        <th scope="col">Amount</th>
+        <th scope="col">Quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+        {props.propData.map(e =><tr key={e._id}>
+                   <td data-label="Food Name"><span style={{marginLeft:"10px"}}>{e.FoodName}</span></td>
+                   <td data-label="Venue Name">{e.VenueName}</td>
+                    <td data-label="Amount">{e.Price}</td>
+                   <td data-label="Quantity">1</td>
+      </tr>      
+            )}
+    
+    </tbody>
+  </table>
+  )
+  
+  
+  
+
+
+
+
 
 export class FloatFooter extends Component {
     constructor(props) {
@@ -17,13 +55,37 @@ export class FloatFooter extends Component {
     render() {
          const data = this.props.badgeData
         return (
-            <div className="floatfooter">
-                <div>{data._id}</div>
+            <PopupState variant="popover" popupId="demo-popup-popover">
+      {(popupState) => (
+        <div className="floatfooter" >
+          
+          <div className="floatfooter" {...bindTrigger(popupState)}>
+                {console.log(data)}
                 <Badge className="cool" badgeContent={data.length} color="secondary" showZero>
                             <ShoppingCartIcon/>  
                           </Badge>
                           
             </div>
+    
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+          >
+            <Box  >     
+            <Table propData={data}/>
+            </Box>
+          </Popover>
+        </div>
+      )}
+    </PopupState>
+            
         )
     }
 }
